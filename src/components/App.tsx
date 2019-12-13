605d529c13c51e96cf66c98fe7603dacf460e9db
Sota Makino <sm96kyoto@gmail.com>
2019-12-14 00:21:37 +0900

    fix code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,15 +5,15 @@ import { images } from '../constants/images';
 
 const imageNodes = images.map(image => <img key={image.id} src={image.src} />);
 const imageCache = images.map(image => (
-  <img key={image.id} src={image.src} style={{ visibility: 'hidden', overflow: 'hidden' }} />
+  <img key={image.id} src={image.src} style={{ display: 'none' }} />
 ));
 
 const App = () => {
   const [volume, setVolume] = React.useState<number>(0);
-
   const handleOnChange = (value: number) => {
     setVolume(value);
   };
+
   return (
     <div style={{ position: 'fixed' }}>
       <div style={{ height: '50px', width: '100px', userSelect: 'none' }}>
@@ -21,7 +21,7 @@ const App = () => {
         {imageCache}
       </div>
       <div style={{ width: '500px', marginLeft: '50px' }}>
-        <Slider value={volume} max={93} tooltip={false} onChange={handleOnChange} />
+        <Slider value={volume} max={images.length} tooltip={false} onChange={handleOnChange} />
       </div>
     </div>
   );
