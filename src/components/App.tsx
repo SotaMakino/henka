3ea93539a98cd0d5e4eeff82544d5d1490a995a1
Sota Makino <sm96kyoto@gmail.com>
2019-12-14 20:18:48 +0900

    deploy test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,24 +2,47 @@ import React from 'react';
 import Slider from 'react-rangeslider';
 import 'react-rangeslider/lib/index.css';
 import { images } from '../constants/images';
+import ImgPreloader from 'react-img-preloader';
 
-const imageNodes = images.map(image => <img key={image.id} src={image.src} />);
-const imageCache = images.map(image => <img key={image.id} src={image.src} />);
+const imageNodes = images.map(image => <img src={image.src} alt="" />);
+const imageCaches = images.map(image => <img src={image.src} style={{ display: 'none' }} alt="" />);
 
 const App = () => {
   const [volume, setVolume] = React.useState<number>(0);
-  const handleOnChange = (value: number) => {
+  const handleOnChange = React.useCallback((value: number) => {
     setVolume(value);
-  };
+  }, []);
+  const preloadImages = React.useMemo(() => images.map(image => image.src), []);
+  const myImage = React.useMemo(
+    () => (index: number) => (volume === index ? imageNodes[index] : imageCaches[index]),
+    [volume]
+  );
 
   return (
     <div style={{ position: 'fixed' }}>
+      <ImgPreloader
+        imgs={preloadImages}
+        onComplete={() => {
+          console.log('All images are loaded');
+        }}
+      >
+        {({ loaded, total }: any) => (
+          <div>
+            {loaded}/{total}
+          </div>
+        )}
+      </ImgPreloader>
       <div style={{ height: '50px', width: '100px', userSelect: 'none' }}>
-        {imageNodes[volume]}
-        {imageCache}
+        {myImage(volume - 3)}
+        {myImage(volume - 2)}
+        {myImage(volume - 1)}
+        {myImage(volume)}
+        {myImage(volume + 1)}
+        {myImage(volume + 2)}
+        {myImage(volume + 3)}
       </div>
       <div style={{ width: '500px', marginLeft: '50px' }}>
-        <Slider value={volume} max={images.length} tooltip={false} onChange={handleOnChange} />
+        <Slider value={volume} max={imageNodes.length} tooltip={false} onChange={handleOnChange} />
       </div>
     </div>
   );
