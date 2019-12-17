5530c0330c1b56ba256528d038a48f5a68c37f3c
Sota Makino <sm96kyoto@gmail.com>
2019-12-18 03:02:16 +0900

    add top page

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { RowTitle, KonaTitle, PlaTitle } from '../constants/Images';
 
 const Top = () => {
   return (
-    <>
-      <Link to="/row">row</Link>
-      <Link to="/kona">kona</Link>
-      <Link to="/pla">pla</Link>
-    </>
+    <Wrapper>
+      <ImageWrapper to="/row">
+        <Image src={RowTitle} />
+      </ImageWrapper>
+      <ImageWrapper to="/kona">
+        <Image src={KonaTitle} />
+      </ImageWrapper>
+      <ImageWrapper to="/pla">
+        <Image src={PlaTitle} />
+      </ImageWrapper>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 60px;
+`;
+const ImageWrapper = styled(Link)`
+  height: 33%;
+`;
+const Image = styled.img`
+  width: 60%;
+`;
 
 export default Top;
